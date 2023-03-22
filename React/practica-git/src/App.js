512d95562df4da33d17a551cd02fa47ec12b1fba
Aleksandar Halachev <alekbalek1@gmail.com>
2023-03-22 19:33:05 +0000

    aa

--- React/practica-git/src/App.js
+++ React/practica-git/src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const mensaje = "Probando git";
+  const mensaje = "Probando git 2";
   console.log(mensaje);
   return (
     <div className="App">
