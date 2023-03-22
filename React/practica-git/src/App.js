66e0cba20124660ea478c8969544cc4eadb2eb25
Aleksandar Halachev <alekbalek1@gmail.com>
2023-03-22 19:15:52 +0000

    commit en el grande

--- React/practica-git/src/App.js
+++ React/practica-git/src/App.js
@@ -1,7 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const mensaje = "Probando git";
+  console.log(mensaje);
   return (
     <div className="App">
       <header className="App-header">
