798acdf20ee7457fc8a034f0b5e1673ca7dba56a
Aleksandar Halachev <alekbalek1@gmail.com>
2023-03-24 12:25:50 +0000

    aa

--- React/practica-git/src/components/Inicio.jsx
+++ React/practica-git/src/components/Inicio.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Inicio = () => {
-  return <div>Inicio modificado en rama componentes</div>;
+  return <div>En rama componentes</div>;
 };
 
 export default Inicio;
