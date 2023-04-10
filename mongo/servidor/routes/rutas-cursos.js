c7088e7c68f9b39a2ba044335c6958a195c0e4b0
Aleksandar Halachev <alekbalek1@gmail.com>
2023-04-10 12:43:47 +0100

    mogoose server

--- mongo/servidor/routes/rutas-cursos.js
+++ mongo/servidor/routes/rutas-cursos.js
@@ -17,6 +17,7 @@ router.get("/", async (req, res, next) => {
   });
 });
 
+// * Recuperar un curso en base a su id desde la BDD en Atlas
 router.get("/:id", async (req, res, next) => {
   const idCurso = req.params.id;
   let curso;
@@ -30,6 +31,66 @@ router.get("/:id", async (req, res, next) => {
     return next(error);
   }
   res.status(200).json({
+    curso: curso,
+  });
+});
+
+// * Como crear un nuevo curso y guardarlo en la BDD
+router.post("/", async (req, res, next) => {
+  const { curso, docente, precio } = req.body; // destructuring
+  // Nuevo documento basado en el Modelo del Curso
+  const nuevoCurso = new Curso({
+    curso,
+    docente,
+    precio,
+  });
+  try {
+    await nuevoCurso.save(); // Guardar en MongoDB Atlas
+  } catch (error) {
+    const err = new Error("No se han podido guardar los datos");
+    err.code = 500;
+    return next(err);
+  }
+  res.status(201).json({
+    curso: nuevoCurso,
+  });
+});
+
+router.put("/:id", async (req, res, next) => {
+  const camposPorCambiar = req.body;
+  const idCurso = req.params.id;
+  let cursoBuscar;
+  try {
+    cursoBuscar = await Curso.findByIdAndUpdate(idCurso, camposPorCambiar, {
+      new: true,
+      runValidators: true,
+    });
+  } catch (error) {
+    res.status(404).json({
+      mensaje: "No se han podido actualizar los datos del curso",
+      error: error.message,
+    });
+    return next(error);
+  }
+  res.status(200).json({
+    mensaje: "Datos de curso modificados",
+    curso: cursoBuscar,
+  });
+});
+
+router.delete("/:id", async (req, res, next) => {
+  let curso;
+  try {
+    curso = await Curso.findByIdAndDelete(req.params.id);
+  } catch (error) {
+    res.status(404).json({
+      mensaje: "No se ha podido eliminar el curso con ese id",
+      error: error.message,
+    });
+    return next(error);
+  }
+  res.status(204).json({
+    mensaje: "Curso eliminado",
     curso: curso,
   });
 });
