897464e44c5ac25f94a4f170da05f413ea84b75f
Aleksandar Halachev <alekbalek1@gmail.com>
2023-04-18 09:08:30 +0100

    ae2 uf1846

---
+++ proyectoFinal/ae2_uf184_Alek/routes/rutas-usuarios.js
@@ -0,0 +1,117 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+
+const Usuario = require("../models/usuarios-model");
+
+router.get("/", async (req, res, next) => {
+  let usuarios;
+  try {
+    usuarios = await Usuario.find({}, "-password");
+  } catch (err) {
+    const error = new Error(
+      "Error. No se han podido recuperar los datos de los usuarios."
+    );
+    error.code = 500;
+    return next(error);
+  }
+  res.status(200).json({
+    mensaje: "Todos los usuarios:",
+    usuarios: usuarios,
+  });
+});
+
+router.get("/:id", async (req, res, next) => {
+  let usuario;
+  const idUsuario = req.params.id;
+  try {
+    usuario = await Usuario.findById(idUsuario);
+  } catch (err) {
+    const error = new Error("Error en la recuperación de datos");
+    error.code = 500;
+    return next(error);
+  }
+  if (!usuario) {
+    const error = new Error("No se ha encontrado el usuario");
+    error.code = 404;
+    return next(error);
+  }
+  res.status(200).json({
+    mensaje: "Usuario:",
+    usuario: usuario,
+  });
+});
+
+router.post("/", async (req, res, next) => {
+  const { nombre, email, password } = req.body;
+  let existeUsuario;
+  try {
+    existeUsuario = await Usuario.findOne({ email: email });
+  } catch (err) {
+    const error = new Error("Email ya existe");
+    error.code = 500;
+    return next(error);
+  }
+
+  if (existeUsuario) {
+    const error = new Error("Ya existe un usuario con ese email");
+    error.code = 401;
+    return next(error);
+  } else {
+    let hashedPassword;
+    try {
+      hashedPassword = await bcrypt.hash(password, 12);
+    } catch (error) {
+      const err = new Error("Error en la encripción de la contraseña");
+      err.code = 500;
+      return next(err);
+    }
+    const nuevoUsuario = new Usuario({
+      nombre,
+      email,
+      password: hashedPassword,
+    });
+    try {
+      await nuevoUsuario.save();
+    } catch (error) {
+      const err = new Error("Error al guardar el usuario");
+      err.code = 500;
+      return next(err);
+    }
+    res.status(201).json({
+      mensaje: "Usuario creado",
+      usuario: nuevoUsuario,
+    });
+  }
+});
+
+router.post("/login", async (req, res, next) => {
+  const { email, password } = req.body;
+  let usuarioExiste;
+  try {
+    usuarioExiste = await Usuario.findOne({ email: email });
+  } catch (err) {
+    const error = new Error("No se puede realizar la operación");
+    error.code = 500;
+    return next(error);
+  }
+
+  if (!usuarioExiste) {
+    const error = new Error("Email no existe");
+    error.code = 422;
+    return next(error);
+  }
+
+  let passwordValido = false;
+  passwordValido = bcrypt.compareSync(password, usuarioExiste.password);
+  if (!passwordValido) {
+    const error = new Error("Contraseña incorrecta");
+    error.code = 401;
+    return next(error);
+  }
+  res.json({
+    mensaje: "Usuario autenticado",
+    email: usuarioExiste.email,
+  });
+});
+module.exports = router;
