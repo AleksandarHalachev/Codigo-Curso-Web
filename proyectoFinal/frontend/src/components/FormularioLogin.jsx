de7432db8f29d11fb8435a18abe6f6db25fc78c3
Aleksandar Halachev <alekbalek1@gmail.com>
2023-04-21 13:41:56 +0100

    adding login form functionality

--- proyectoFinal/frontend/src/components/FormularioLogin.jsx
+++ proyectoFinal/frontend/src/components/FormularioLogin.jsx
@@ -1,16 +1,92 @@
 import React from "react";
 import "./FormularioLogin.css";
+import { useForm } from "react-hook-form";
+import axios from "axios";
 
 const FormularioLogin = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const gestorFormulario = async (data) => {
+    await axios
+      .post("http://localhost:5000/api/usuarios/login", {
+        email: data.email,
+        password: data.password,
+      })
+      .then((response) => {
+        console.log("Todo correcto", response.data);
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+      });
+  };
+
   return (
     <div>
       <div className="form-box">
-        <form className="form">
+        <form className="form" onSubmit={handleSubmit(gestorFormulario)}>
           <span className="title">Iniciar Sesión</span>
           <span className="subtitle">Inicia sesión con tu email.</span>
           <div className="form-container">
-            <input type="email" className="input" placeholder="Email" />
-            <input type="password" className="input" placeholder="Contraseña" />
+            <input
+              type="email"
+              className="input"
+              placeholder="Email"
+              {...register(
+                "email",
+                {
+                  pattern:
+                    /^(?![_.-])((?![_.-][_.-])[a-zA-Z\d_.-]){0,63}[a-zA-Z\d]@((?!-)((?!--)[a-zA-Z\d-]){0,63}[a-zA-Z\d]\.){1,2}([a-zA-Z]{2,14}\.)?[a-zA-Z]{2,14}$/,
+                },
+                { minLength: 3 },
+                {
+                  required: true,
+                  message: "Email requerido",
+                }
+              )}
+            />
+            {errors.email &&
+              errors.email.type === "required" &&
+              "Campo email requerido"}
+            {errors.email &&
+              errors.email.type === "minLength" &&
+              "Longitud mínima de 3 caracteres."}
+            {errors.email &&
+              errors.email.type === "pattern" &&
+              "Formato de email incorrecto"}
+
+            <input
+              type="password"
+              className="input"
+              placeholder="Contraseña"
+              {...register(
+                "password",
+                {
+                  pattern:
+                    /^(?=.*[0-9!@#$%^&*()_+=[\]{};':"|,.<>/?])(?=.*[A-Z]).+$/,
+                },
+                { minLength: 6, maxLength: 32 },
+                {
+                  required: true,
+                  message: "Contraseña requerida",
+                }
+              )}
+            />
+            {errors.password &&
+              errors.password.type === "minLength" &&
+              "Longitud mínima de la contraseña de 6 caracteres"}
+            {errors.password &&
+              errors.password.type === "maxLength" &&
+              "Longitud máxima de la contraseña de 32 caracteres"}
+            {errors.password &&
+              errors.password.type === "required" &&
+              "Contraseña requerida"}
+            {errors.password &&
+              errors.password.type === "pattern" &&
+              "Formato de contraseña incorrecto. Mínimo un símbolo y una mayúscula"}
           </div>
           <button>Iniciar sesión</button>
         </form>
